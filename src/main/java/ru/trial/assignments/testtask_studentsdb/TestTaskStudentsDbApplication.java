b1a95b051f9d1936408c5923ca5ddfc47eba9f35
Nuby <nuby.4register@gmail.com>
2025-01-21 14:05:07 +0300

    init

---
+++ src/main/java/ru/trial/assignments/testtask_studentsdb/TestTaskStudentsDbApplication.java
@@ -0,0 +1,13 @@
+package ru.trial.assignments.testtask_studentsdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskStudentsDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskStudentsDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/trial/assignments/testtask_studentsdb/TestTaskStudentsDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.trial.assignments.testtask_studentsdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskStudentsDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
