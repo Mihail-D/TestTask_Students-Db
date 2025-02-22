0e96b019484b21a276f8ef7191522af97265b225
Nuby <nuby.4register@gmail.com>
2025-02-22 20:03:23 +0300

    add: Student package

--- src/main/java/ru/trial/assignments/testtask_studentsdb/student/dto/StudentDto.java
+++ src/main/java/ru/trial/assignments/testtask_studentsdb/student/dto/StudentDto.java
@@ -1,9 +1,5 @@
 package ru.trial.assignments.testtask_studentsdb.student.dto;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,34 +14,27 @@ import java.time.LocalDate;
 @Builder
 public class StudentDto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull(message = "First name cannot be null.")
     @NotBlank(message = "First name cannot be empty or contain spaces.")
     @Size(min = 1, max = 50, message = "First name must be between 1 and 50 characters.")
-    @Column(name = "first_name", nullable = false)
     private String firstName;
 
     @NotNull(message = "Last name cannot be null.")
     @NotBlank(message = "Last name cannot be empty or contain spaces.")
     @Size(min = 1, max = 50, message = "Last name must be between 1 and 50 characters.")
-    @Column(name = "last_name", nullable = false)
     private String lastName;
 
     @Size(min = 1, max = 50, message = "Middle name must be between 1 and 50 characters.")
-    @Column(name = "middle_name")
     private String middleName;
 
     @NotNull(message = "Birth Date cannot be null.")
     @Past(message = "Birth Date must be in the past.")
-    @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
 
     @NotNull(message = "Group ID cannot be null.")
     @Positive(message = "Group ID must be positive.")
-    @Column(name = "group_id")
     private Long groupId;
 
     public StudentDto(Long id, String firstName, String lastName, String middleName, LocalDate birthDate, Long groupId) {
